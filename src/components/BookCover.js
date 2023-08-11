d8202f3049b88ab076c79c53f11be123b0cc0568
rdrn-abs <rdrn@abstractit.in>
2023-08-11 11:20:42 +0530

    Book cover flip.

---
+++ src/components/BookCover.js
@@ -0,0 +1,24 @@
+import React from 'react';
+const BookCover = () =>
+	<div className="holder">
+		<div className="title">Book cover</div>
+		<div className="book">
+			<div className="bookCover">
+				<div>
+					<h2>Wonders of CSS</h2>
+					<p>Author name</p>
+				</div>
+
+			</div>
+			<div className="bookContent">
+				<h2>Chapter 1</h2>
+				<p>Lorem epsom Lorem epsomLorem epsomLorem epsomLorem
+					epsomLorem epsomLorem epsomLorem epsomLorem epsomLorem
+					epsomLorem epsomLorem epsomLorem epsomLorem epsomLorem
+					epsomLorem epsomLorem epsomLorem epsom.
+				</p>
+			</div>
+		</div>
+	</div>;
+
+export default BookCover;
